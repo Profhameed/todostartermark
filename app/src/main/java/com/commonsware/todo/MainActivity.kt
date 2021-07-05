02e75db3b889b586ecafa9d816c9fac74e751756
Abdul Hameed <profhameed@outlook.com>
2021-07-06 00:38:01 +0500

    after menu has been inserted

--- app/src/main/java/com/commonsware/todo/MainActivity.kt
+++ app/src/main/java/com/commonsware/todo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.commonsware.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.commonsware.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.todo_roster)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actions,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 }
